59422b4ccbd62949f8fda5fdf181363a647cddd2
reverSilly <75513686+reverSilly@users.noreply.github.com>
2022-05-08 14:00:03 +0800

    Update msgpack11.hpp

--- msgpack11.hpp
+++ msgpack11.hpp
@@ -70,28 +70,50 @@ namespace msgpack11
 		// Array and object typedefs
 		using array=std::deque<MsgPack>;
 		using object=std::unordered_map<MsgPack,MsgPack>;
-		
+		using string=std::string;
+		//floats
+		using float32=float;
+		using float64=double;
+		//boolean
+		using boolean=bool;
 		// Binary and extension typedefs
 		using binary=std::vector<uint8_t>;
 		using extension=std::tuple<uint8_t,binary>;
 		
+		//integers
+		using int8=int8_t;
+		using int16=int16_t;
+		using int32=int32_t;
+		using int64=int64_t;
+		using uint8=uint8_t;
+		using uint16=uint16_t;
+		using uint32=uint32_t;
+		using uint64=uint64_t;
+		using int128=__int128;
+		
 		// Constructors for the various types of JSON value.
-		MsgPack() noexcept;                // NUL
-		MsgPack(std::nullptr_t) noexcept;  // NUL
-		MsgPack(float value);              // FLOAT32
-		MsgPack(double value);             // FLOAT64
-		MsgPack(int8_t value);             // INT8
-		MsgPack(int16_t value);            // INT16
-		MsgPack(int32_t value);            // INT32
-		MsgPack(int64_t value);            // INT64
-		MsgPack(uint8_t value);            // UINT8
-		MsgPack(uint16_t value);           // UINT16
-		MsgPack(uint32_t value);           // UINT32
-		MsgPack(uint64_t value);           // UINT64
+		MsgPack();                         // NUL
+		MsgPack(std::nullptr_t);           // NUL
+//		template<typename T> requires(std::is_fundamental_v<T>)
+//		MsgPack(T value);
+//		
+//		template<typename T> requires(std::is_class_v<T>)
+//		MsgPack(const T& value);
+//		template<typename T> requires(std::is_class_v<T>)
+//		MsgPack(T&& value);
+		MsgPack(float32 value);              // FLOAT32
+		MsgPack(float64 value);             // FLOAT64
+		MsgPack(int8 value);             // INT8
+		MsgPack(int16 value);            // INT16
+		MsgPack(int32 value);            // INT32
+		MsgPack(int64 value);            // INT64
+		MsgPack(uint8 value);            // UINT8
+		MsgPack(uint16 value);           // UINT16
+		MsgPack(uint32 value);           // UINT32
+		MsgPack(uint64 value);           // UINT64
 		MsgPack(bool value);               // BOOL
-		MsgPack(const std::string &value); // STRING
-		MsgPack(std::string &&value);      // STRING
-		MsgPack(const char * value);       // STRING
+		MsgPack(const string &value);      // STRING
+		MsgPack(string &&value);           // STRING
 		MsgPack(const array &values);      // ARRAY
 		MsgPack(array &&values);           // ARRAY
 		MsgPack(const object &values);     // OBJECT
@@ -102,53 +124,45 @@ namespace msgpack11
 		MsgPack(extension &&values);       // EXTENSION
 		
 		// Implicit constructor: anything with a to_msgpack() function.
-		template <class T> requires
-		requires(T thing)
-		{
-			MsgPack(thing.to_msgpack());
-		}
+		template <class T> requires requires(T thing){MsgPack(thing.to_msgpack());}
 		MsgPack(const T & t) : MsgPack(t.to_msgpack()) {}
 		
 		// Implicit constructor: map-like objects (std::map, std::unordered_map, etc)
-		template <class M
-//		,typename std::enable_if<
-//		std::is_constructible_v<MsgPack, typename M::key_type>
-//		&& std::is_constructible_v<MsgPack, typename M::mapped_type>,
-//		int>::type = 0
-		> requires
-		requires(M map)
-		{
-			typename M::key_type;
-			typename M::mapped_type;
-		}&&
-		requires(typename M::key_type key){MsgPack(key);}&&
-		requires(typename M::mapped_type value){MsgPack(value);}
+		template <class M> requires
+		(
+			requires(M map)
+			{
+				typename M::key_type;
+				typename M::mapped_type;
+			}&&
+			requires(typename M::key_type key){MsgPack(key);}&&
+			requires(typename M::mapped_type value){MsgPack(value);}&&
+			!std::is_same_v<M,object>
+		)
 		MsgPack(const M & m) : MsgPack(object(std::begin(m),std::end(m))) {}
 		
 		// Implicit constructor: vector-like objects (std::list, std::vector, std::set, etc)
-		template <class V
-//		,typename std::enable_if<
-//		std::is_constructible_v<MsgPack, typename V::value_type>&&
-//		!std::is_same_v<typename binary::value_type, typename V::value_type>,
-//		int>::type=0
-		>requires
+		template <class V>requires
 		(
 			requires(V arr){typename V::value_type;}&&
 			requires(typename V::value_type value){MsgPack(value);}&&
-			!std::is_same_v<typename binary::value_type, typename V::value_type>
+			!std::is_same_v<typename binary::value_type, typename V::value_type>&&
+			!std::is_same_v<V,array>&&
+			!std::is_same_v<V,string>&&
+			!std::is_same_v<V,binary>&&
+			!std::is_same_v<V,object>
 		)
 		MsgPack(const V & v):MsgPack(array(std::begin(v),std::end(v))){}
 		
-		template <class V
-//		,typename std::enable_if<
-//		std::is_constructible<MsgPack, typename V::value_type>::value &&
-//		std::is_same<typename binary::value_type, typename V::value_type>::value,
-//		int>::type = 0
-		>requires
+		template <class V>requires
 		(
 			requires(V arr){typename V::value_type;}&&
 			requires(typename V::value_type value){MsgPack(value);}&&
-			std::is_same_v<typename binary::value_type,typename V::value_type>
+			std::is_same_v<typename binary::value_type,typename V::value_type>&&
+			!std::is_same_v<V,binary>&&
+			!std::is_same_v<V,array>&&
+			!std::is_same_v<V,string>&&
+			!std::is_same_v<V,object>
 		)
 		MsgPack(const V & v):MsgPack(binary(std::begin(v),std::end(v))) {}
 		
@@ -160,7 +174,7 @@ namespace msgpack11
 		Type type() const;
 		
 		bool is_null()      const { return type() == Type::NUL; }
-		bool is_bool()      const { return type() == Type::BOOL; }
+		bool is_boolean()   const { return type() == Type::BOOL; }
 		bool is_number()    const { return static_cast<uint8_t>(type())&static_cast<uint8_t>(Type::NUMBER); }
 		bool is_float32()   const { return type() == Type::FLOAT32; }
 		bool is_float64()   const { return type() == Type::FLOAT64; }
@@ -182,36 +196,36 @@ namespace msgpack11
 		// Return the enclosed value if this is a number, 0 otherwise. Note that msgpack11 does not
 		// distinguish between integer and non-integer numbers - number_value() and int_value()
 		// can both be applied to a NUMBER-typed object.
-		double number_value() const;
-		float float32_value() const;
-		double float64_value() const;
-		int32_t int_value() const;
-		int8_t int8_value() const;
-		int16_t int16_value() const;
-		int32_t int32_value() const;
-		int64_t int64_value() const;
-		uint8_t uint8_value() const;
-		uint16_t uint16_value() const;
-		uint32_t uint32_value() const;
-		uint64_t uint64_value() const;
 		
-		// Return the enclosed value if this is a boolean, false otherwise.
-		bool bool_value() const;
-		// Return the enclosed string if this is a string, "" otherwise.
-		const std::string &string_value() const;
-		// Return the enclosed std::vector if this is an array, or an empty vector otherwise.
-		const array &array_items() const;
-		// Return the enclosed std::map if this is an object, or an empty map otherwise.
-		const object &object_items() const;
-		// Return the enclosed std::vector if this is an binary, or an empty map otherwise.
-		const binary &binary_items() const;
-		// Return the enclosed std::tuple if this is an extension, or an empty map otherwise.
-		const extension &extension_items() const;
+		template<typename T> requires(!std::is_const_v<T>)
+		explicit operator T&();
+		template<typename T> requires(!std::is_const_v<T>)
+		T& as(){return operator T&();}
+		
+		// Return the enclosed value if this is a number, throws exception otherwise. Note that msgpack11 does not
+		// distinguish between integer and non-integer numbers - number_value() and int_value()
+		// can both be applied to a NUMBER-typed object.
+		
+		template<typename T> requires(std::is_fundamental_v<T>)
+		explicit operator T() const;
+		template<typename T> requires(std::is_fundamental_v<T>)
+		T as() const{return operator T();}
+		
+		template<typename T> requires(!std::is_fundamental_v<T>)
+		explicit operator const T&() const;
+		template<typename T> requires(!std::is_fundamental_v<T>)
+		const T& as() const{return operator const T&();}
+		
+		//cast for immutable types
 		
 		// Return a reference to arr[i] if this is an array, MsgPack() otherwise.
-		const MsgPack & operator[](size_t i) const;
+		const MsgPack& operator[](size_t i) const;
+		// Return a mutable reference to arr[i] if this is an array, MsgPack() otherwise.
+		MsgPack& operator[] (size_t i);
 		// Return a reference to obj[key] if this is an object, MsgPack() otherwise.
-		const MsgPack & operator[](const std::string &key) const;
+		const MsgPack& operator[](const MsgPack &key) const;
+		// Return a referenct to obj[key] if this is an object and obj[key] exists, creating a new member otherwise.
+		MsgPack& operator[](const MsgPack &key);
 		
 		// Serialize.
 		void dump(std::string &out) const
@@ -262,7 +276,8 @@ namespace msgpack11
 		
 		static inline std::vector<MsgPack> parse_multi(
 			const std::string & in,
-			std::string & err) {
+			std::string & err)
+		{
 			std::string::size_type parser_stop_pos;
 			return parse_multi(in, parser_stop_pos, err);
 		}
@@ -275,11 +290,6 @@ namespace msgpack11
 		bool operator<= (const MsgPack &rhs) const=default;
 		bool operator>  (const MsgPack &rhs) const=default;
 		bool operator>= (const MsgPack &rhs) const=default;
-//		bool operator<  (const MsgPack &rhs) const;
-//		bool operator!= (const MsgPack &rhs) const { return !(*this == rhs); }
-//		bool operator<= (const MsgPack &rhs) const { return !(rhs < *this); }
-//		bool operator>  (const MsgPack &rhs) const { return  (rhs < *this); }
-//		bool operator>= (const MsgPack &rhs) const { return !(*this < rhs); }
 		
 		/* has_shape(types, err)
      *
